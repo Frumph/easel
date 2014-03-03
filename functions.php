9f0a5fed70073588e329b740e9f2aa5308c8b858
frumph <philip@frumph.net>
2014-03-02 17:12:19 -0800

    word change for youtube line

--- functions.php
+++ functions.php
@@ -262,7 +262,7 @@ if (!function_exists('easel_display_social_icons')) {
 		if (!empty($email)) $output .= '<a href="'.$email.'" target="_blank" title="'.__('Email me','easel').'" class="menunav-social menunav-email">'.__('Email','easel').'</a>'."\r\n";
 		if (!empty($googleplus)) $output .= '<a href="'.$googleplus.'" target="_blank" title="'.__('Circle me on Google+','easel').'" class="menunav-social menunav-googleplus">'.__('Google+','easel').'</a>'."\r\n";
 		if (!empty($pinterest)) $output .= '<a href="'.$pinterest.'" target="_blank" title="'.__('Peruse my Pinterests','easel').'" class="menunav-social menunav-pinterest">'.__('pinterest','easel').'</a>'."\r\n";
-		if (!empty($youtube)) $output .= '<a href="'.$youtube.'" target="_blank" title="'.__('View my YouTube-Channel','easel').'" class="menunav-social menunav-youtube">'.__('YouTube','easel').'</a>'."\r\n";
+		if (!empty($youtube)) $output .= '<a href="'.$youtube.'" target="_blank" title="'.__('My Channel on YouTube','easel').'" class="menunav-social menunav-youtube">'.__('YouTube','easel').'</a>'."\r\n";
 		if (easel_themeinfo('enable_rss_in_menubar')) $output .= '<a href="'.get_bloginfo('rss2_url').'" target="_blank" title="'.__('RSS Feed','easel').'" class="menunav-social menunav-rss2">'.__('RSS','easel').'</a>'."\r\n";
 		$output .= '<div class="clear"></div>';
 		$output .= '</div>'."\r\n";
