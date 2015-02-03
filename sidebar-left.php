3c1c67c1e9423571dbd5cf77bc6511e4025ca5b5
Kniebremser <kniebremser@fluchtsportler.de>
2015-02-03 22:48:13 +0100

    fixed sidebars
    delete Recommended Plugins

--- sidebar-left.php
+++ sidebar-left.php
@@ -4,13 +4,15 @@
 		do_action('easel-sidebar-left');
 		if ( !dynamic_sidebar('sidebar-left-sidebar') ) { ?>
 		<div class="sidebar-no-widgets">
-			<?php _e('There are currently no widgets assigned to the left-sidebar, place some!','easel'); ?><br />
+			<?php _e('There are currently no widgets assigned to the left-sidebar, place some!', 'easel'); ?>
 			<br />
-			<?php _e('Once you add widgets to this sidebar, this default information will go away.','easel'); ?><br />
 			<br />
-			<?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.','easel'); ?><br />
+			<?php _e('Once you add widgets to this sidebar, this default information will go away.', 'easel'); ?>
+			<br />
+			<br />
+			<?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.', 'easel'); ?>
+			<br />
 		</div>
-		<?php }
-	?>
+		<?php } ?>
 	</div>
 </div>
--- sidebar-right.php
+++ sidebar-right.php
@@ -4,17 +4,15 @@
 		do_action('easel-sidebar-right');
 		if ( !dynamic_sidebar('sidebar-right-sidebar') ) { ?>
 		<div class="sidebar-no-widgets">
-			<?php _e('There are currently no widgets assigned to the right-sidebar, place some!', 'easel'); ?><br />
+			<?php _e('There are currently no widgets assigned to the right-sidebar, place some!', 'easel'); ?>
 			<br />
-			<?php _e('Once you add widgets to this sidebar, this default information will go away.', 'easel'); ?><br />
 			<br />
-			<?php _e('This theme also uses the WordPress 3.0 Menu system. You probably see the default stuff you have in the menubar. Go to Appearance &#10132; Menu in the wp-admin (dashboard) and create a new menu.', 'easel'); ?><br />
-		</div>
-			<h2><?php _e('Recommended Plugins', 'easel'); ?></h2>
-			<ol>
-				<li><a href="https://wordpress.org/plugins/comic-easel/">Comic Easel</a></li>
-				<li><a href="https://wordpress.org/plugins/vipers-video-quicktags/">Viper's Video Quicktags</a></li>
-			</ol>
+			<?php _e('Once you add widgets to this sidebar, this default information will go away.', 'easel'); ?>
+			<br />
+			<br />
+			<?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.', 'easel'); ?>
+		    <br />
+		 </div>
 		<?php } ?>
 	</div>
 </div>
