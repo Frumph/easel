f9eb047839e3bcb1b5d1a73f1b822d4e2a91025e
Kniebremser <kniebremser@fluchtsportler.de>
2015-09-18 11:44:13 +0200

    changed formatting error

--- functions/customize.php
+++ functions/customize.php
@@ -17,18 +17,18 @@ function eaeel_sanitize_checkbox( $input ) {
 
 class easel_Customize {
 
-/**
- * This hooks into 'customize_register' (available as of WP 3.4) and allows
- * you to add new sections and controls to the Theme Customize screen.
- * 
- * Note: To enable instant preview, we have to actually write a bit of custom
- * javascript. See live_preview() for more.
- * 
- * @see add_action('customize_register',$func)
- * @param \WP_Customize_Manager $wp_customize
- * @link http://ottopress.com/2012/how-to-leverage-the-theme-customizer-in-your-own-themes/
- * @since Easel
- */
+	/**
+ 	* This hooks into 'customize_register' (available as of WP 3.4) and allows
+	* you to add new sections and controls to the Theme Customize screen.
+ 	* 
+ 	* Note: To enable instant preview, we have to actually write a bit of custom
+	* javascript. See live_preview() for more.
+ 	* 
+ 	* @see add_action('customize_register',$func)
+ 	* @param \WP_Customize_Manager $wp_customize
+ 	* @link http://ottopress.com/2012/how-to-leverage-the-theme-customizer-in-your-own-themes/
+ 	* @since Easel
+ 	*/
 
 	public $css_array = Array();
 	
