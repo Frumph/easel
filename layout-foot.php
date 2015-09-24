c71fbce84cd01220352bffac1bda409557399efd
Kniebremser <kniebremser@fluchtsportler.de>
2015-09-25 00:01:28 +0200

    fixed error of display spacebar.png

--- layout-foot.php
+++ layout-foot.php
@@ -1,4 +1,4 @@
-				<div class="spacer"><img src="<? echo get_template_directory_uri(); ?>/images/spacer.png" /></div>
+				<div class="spacer"><img src=<?php echo get_template_directory_uri(). '/images/spacer.png' ?>></div>
 				<?php if (!easel_sidebars_disabled()) easel_get_sidebar('under-blog'); ?>
 			</div>
 <?php 
